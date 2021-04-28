390fd929587bcf36fd737f91c9e1ce5bae26d7d2
Jay T <jaytula@gmail.com>
2021-04-28 04:24:29 -0700

    229. Protecting API Routes

---
+++ pages/api/user/change-password.ts
@@ -0,0 +1,16 @@
+import { NextApiHandler } from "next";
+import { getSession } from "next-auth/client";
+
+const handler: NextApiHandler = async (req, res) => {
+  if(req.method !== 'PATCH') {
+    return;
+  }
+
+  const session = await getSession({req: req})
+  if(!session) {
+    res.status(401).json({message: 'Not authenticated!'});
+    return;
+  }
+}
+
+export default handler
