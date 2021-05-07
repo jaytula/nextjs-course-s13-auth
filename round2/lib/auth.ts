2c6aebf8eecd007f4ac3f66df32c7ba6caf4e51e
Jay T <jaytula@gmail.com>
2021-05-06 18:39:31 -0700

    220. Adding the 'Credentials Auth Provider' & User Login Logic

--- round2/lib/auth.ts
+++ round2/lib/auth.ts
@@ -1,5 +1,9 @@
-import { hash } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
 
-export async function hashPassword(password) {
+export async function hashPassword(password: string) {
   return hash(password, 12);
+}
+
+export async function verifyPassword(password: string, hashedPassword: string) {
+  return compare(password, hashedPassword);
 }
