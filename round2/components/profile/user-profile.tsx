3c3fe28166a32e5a3b6b42dbed5c4dcc06825cf7
Jay T <jaytula@gmail.com>
2021-05-06 21:18:54 -0700

    225. Adding Server-Side Page Guards (And When To Use Which Approach)

--- round2/components/profile/user-profile.tsx
+++ round2/components/profile/user-profile.tsx
@@ -4,19 +4,19 @@ import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  // const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    getSession().then(session => {
-      if(!session) {
-        window.location.href = '/auth'
-      } else {
-        setIsLoading(false);
-      }
-    })
-  }, [])
+  // useEffect(() => {
+  //   getSession().then(session => {
+  //     if(!session) {
+  //       window.location.href = '/auth'
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   })
+  // }, [])
   
-  if(isLoading) return <p className={classes.profile}>Loading...</p>
+  // if(isLoading) return <p className={classes.profile}>Loading...</p>
 
   return (
     <section className={classes.profile}>
--- round2/pages/profile.tsx
+++ round2/pages/profile.tsx
@@ -1,7 +1,26 @@
-import UserProfile from '../components/profile/user-profile';
+import { GetServerSideProps, GetServerSidePropsResult } from "next";
+import UserProfile from "../components/profile/user-profile";
+import { getSession } from "next-auth/client";
 
 function ProfilePage() {
   return <UserProfile />;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (!session)
+    return {
+      props: {},
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+
+  return {
+    props: { session },
+  };
+};
 
 export default ProfilePage;
