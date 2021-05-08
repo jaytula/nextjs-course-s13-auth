4e9979255ff61bf75bb7802f714d7e0454d2aec6
Jay T <jaytula@gmail.com>
2021-05-08 01:24:48 -0700

    230. Adding the 'Change Password' Logic

--- round2/pages/api/user/change-password.ts
+++ round2/pages/api/user/change-password.ts
@@ -16,6 +16,37 @@ const handler: NextApiHandler = async (req, res) => {
     res.status(401).json({message: 'Not authenticated'});
     return;
   }
+
+  const userEmail = session.user.email;
+  const {newPassword, oldPassword} = req.body;
+
+  const client = await connectToDatabase();
+  const usersCollection = await client.db().collection('users');
+
+  const user = await usersCollection.findOne({email: userEmail});
+
+  if(!user) {
+    res.status(404).json({message: 'User not found'})
+    client.close();
+    return;
+  }
+
+  const passwordsAreEqual = await compare(oldPassword, user.password);
+
+  if(!passwordsAreEqual) {
+    res.status(403).json({message: 'Old password does not match'})
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hash(newPassword, 12);
+  const result = await usersCollection.updateOne({email: userEmail}, {$set: { password: hashedPassword}})
+
+  client.close();
+  res.status(201).json({
+    message: 'Password updated',
+    modifiedCount: result.modifiedCount
+  })
 }
 
 export default handler
